09507d343e688b7b054c825711971d050a401a3b
Víctor Hernández <victor.hernandez@qacg.com>
2016-04-06 17:41:29 -0500

    The LoginPresenter class was added

---
+++ app/src/main/java/mx/hercarr/hercarrdroid/presenter/LoginPresenter.java
@@ -0,0 +1,29 @@
+package mx.hercarr.hercarrdroid.presenter;
+
+import mx.hercarr.hercarrdroid.view.ILoginView;
+
+public class LoginPresenter {
+
+    private ILoginView view;
+
+    private static final String LOCAL_USERNAME = "hercarr";
+    private static final String LOCAL_PASSWORD = "secret";
+
+    public LoginPresenter(ILoginView view) {
+        this.view = view;
+    }
+
+    public void validateUser(String username, String password) {
+        if (username != null && !username.isEmpty()
+            && password != null && !password.isEmpty()) {
+            if (username.equals(LOCAL_USERNAME) && password.equals(LOCAL_PASSWORD)) {
+                view.userFound();
+            } else {
+                view.userNotFound();
+            }
+        } else {
+            view.emptyParameters();
+        }
+    }
+
+}
