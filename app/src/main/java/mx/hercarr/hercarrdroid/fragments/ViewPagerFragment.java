45b6cbdca07faaa12a98b7d5eda0cb9e78307bc3
Víctor Hernández <victor.hernandez@qacg.com>
2016-04-22 00:06:25 -0500

    refactoring the view pager section adding the view pager element

--- app/src/main/java/mx/hercarr/hercarrdroid/fragments/ViewPagerFragment.java
+++ app/src/main/java/mx/hercarr/hercarrdroid/fragments/ViewPagerFragment.java
@@ -1,26 +1,26 @@
 package mx.hercarr.hercarrdroid.fragments;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import mx.hercarr.hercarrdroid.R;
 
-public class ViewPagerFragment extends Fragment
-    implements TabLayout.OnTabSelectedListener {
+public class ViewPagerFragment extends Fragment {
 
     private AppBarLayout appBarLayout;
     private TabLayout tabLayout;
-
-    private LocalFriendListFragment localFriendListFragment;
-    private RemoteFriendListFragment remoteFriendListFragment;
+    private ViewPager viewPager;
 
     public ViewPagerFragment() {
 
@@ -34,58 +34,62 @@ public class ViewPagerFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        init();
-        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
+        init(view);
+        return view;
     }
 
-    private void init() {
+    private void init(View view) {
         appBarLayout = (AppBarLayout) getActivity().findViewById(R.id.appBarLayout);
-        localFriendListFragment = LocalFriendListFragment.newInstance();
-        remoteFriendListFragment = RemoteFriendListFragment.newInstance();
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        setViewPager();
         setTabLayout();
-        setTabs();
+        setIcons();
+    }
+
+    public void setViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(LocalFriendListFragment.newInstance());
+        adapter.addFragment(RemoteFriendListFragment.newInstance());
+        viewPager.setAdapter(adapter);
     }
 
     private void setTabLayout() {
         AppBarLayout.LayoutParams layoutParams = new AppBarLayout.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.WRAP_CONTENT);
         tabLayout = new TabLayout(getActivity());
         tabLayout.setLayoutParams(layoutParams);
-        tabLayout.setOnTabSelectedListener(this);
+        tabLayout.setupWithViewPager(viewPager);
         appBarLayout.addView(tabLayout);
     }
 
-    private void setTabs() {
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_list_white_24dp));
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_cloud_white_24dp));
+    public void setIcons() {
+        tabLayout.getTabAt(0).setIcon(R.mipmap.ic_list_white_24dp);
+        tabLayout.getTabAt(1).setIcon(R.mipmap.ic_cloud_white_24dp);
     }
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        fragmentManager
-                .beginTransaction()
-                .replace(R.id.frameVPLayout, fragment)
-                .commit();
-    }
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        int position = tab.getPosition();
-        switch (position) {
-            case 0:
-                replaceFragment(localFriendListFragment);
-                break;
-            case 1:
-                replaceFragment(remoteFriendListFragment);
-                break;
-            default:
-                break;
+        private List<Fragment> fragments;
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragments = new ArrayList<>();
         }
+
+        public void addFragment(Fragment fragment) {
+            fragments.add(fragment);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
     }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {}
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {}
 
 }
